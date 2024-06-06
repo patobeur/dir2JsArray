6d99b85090971ed6ed7b443bc75365ff0df32431
Patobeur41 <48214878+patobeur@users.noreply.github.com>
2024-06-06 10:15:41 +0200

    Update dir2JsArray.bat

--- dir2JsArray.bat
+++ dir2JsArray.bat
@@ -11,15 +11,15 @@ REM Obtenir le nom du dossier parent immédiat
 for %%i in ("%currentDir%") do set "parentFolderName=%%~nxi"
 
 REM Demander à l'utilisateur de saisir un nom pour le fichier
-set "fileNameInput="
-set /p "fileNameInput=Entrez le nom du fichier (ou appuyez sur Entrée pour utiliser '%parentFolderName%'): "
+set "fileNameOutput="
+set /p "fileNameOutput=Entrez le nom du fichier (ou appuyez sur Entrée pour utiliser '%parentFolderName%'): "
 
 REM Utiliser le nom du dossier parent si l'utilisateur appuie simplement sur Entrée
-if "%fileNameInput%"=="" (
-    set "fileNameInput=%parentFolderName%"
+if "%fileNameOutput%"=="" (
+    set "fileNameOutput=%parentFolderName%"
 )
 
-REM Demander à l'utilisateur de saisir une extension de fichier
+REM Demander à l'utilisateur de saisir une extension de fichier pour le choix des fichier à prendre
 set "extensionNameInput="
 set /p "extensionNameInput=Entrez le nom l'extension (ou appuyez sur Entrée pour utiliser 'gltf'): "
 
@@ -28,13 +28,23 @@ if "%extensionNameInput%"=="" (
     set "extensionNameInput=gltf"
 )
 
-echo let !fileNameInput! = [ > !parentFolderName!.js
+REM Demander à l'utilisateur de saisir une extension de fichier pour le fichier final
+set "extensionOutputInput="
+set /p "extensionOutputInput=Entrez le nom l'extension pour votre fichier final (ou appuyez sur Entrée pour utiliser 'js'): "
+
+REM Utiliser l'extension par défaut si l'utilisateur appuie simplement sur Entrée
+if "%extensionOutputInput%"=="" (
+    set "extensionOutputInput=js"
+)
+
+echo let !fileNameOutput! = [ > !parentFolderName!.js
 
 for /r %%i in (*.%extensionNameInput%) do (
     set "filePath=%%i"
     set "fileName=%%~nxi"
-    echo { set: '!fileNameInput!', name: '%%~ni', file: '%%~dpnxi', position: { x: 0, y: 0, z: 0 } }, >> !fileNameInput!.js
+    echo { set: '!fileNameOutput!', name: '%%~ni', file: '%%~dpnxi', position: { x: 0, y: 0, z: 0 } }, >> !fileNameOutput!.!extensionOutputInput!
 )
 
-echo ] >> !fileNameInput!.js
-echo export { '!fileNameInput!' } >> !fileNameInput!.js
+echo ] >> !fileNameOutput!.!extensionOutputInput!
+echo export { '!fileNameOutput!' } >> !fileNameOutput!.!extensionOutputInput!
+
