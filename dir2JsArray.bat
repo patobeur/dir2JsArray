806ccddce057865c464ab8dea861c8852c6643c4
Patobeur41 <48214878+patobeur@users.noreply.github.com>
2024-06-08 15:17:36 +0200

    Update dir2JsArray.bat
    _ added to name object

--- dir2JsArray.bat
+++ dir2JsArray.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
+
 REM Obtenir le chemin complet du dossier contenant le script batch
 set "currentDir=%~dp0"
 
@@ -12,7 +13,7 @@ for %%i in ("%currentDir%") do set "parentFolderName=%%~nxi"
 
 REM Demander à l'utilisateur de saisir un nom pour le fichier
 set "fileNameOutput="
-set /p "fileNameOutput=Entrez le nom du fichier (ou appuyez sur Entrée pour utiliser '%parentFolderName%'): "
+set /p "fileNameOutput=Entrez le nom du Set (ou appuyez sur Entrée pour utiliser le nom du dossier parent '%parentFolderName%'): "
 
 REM Utiliser le nom du dossier parent si l'utilisateur appuie simplement sur Entrée
 if "%fileNameOutput%"=="" (
@@ -23,33 +24,50 @@ REM Demander à l'utilisateur de saisir une extension de fichier pour le choix d
 set "extensionNameInput="
 set /p "extensionNameInput=Entrez le nom l'extension (ou appuyez sur Entrée pour utiliser 'gltf'): "
 
-REM Utiliser le nom du dossier parent si l'utilisateur appuie simplement sur Entrée
+REM Utiliser gltf si vide
 if "%extensionNameInput%"=="" (
     set "extensionNameInput=gltf"
 )
 
-REM Demander à l'utilisateur de saisir une extension de fichier pour le fichier final
+REM Demander à l'utilisateur de saisir une extension de fichier pour le fichier de sortie
 set "extensionOutputInput="
 set /p "extensionOutputInput=Entrez le nom l'extension pour votre fichier final (ou appuyez sur Entrée pour utiliser 'js'): "
 
-REM Utiliser l'extension par défaut si l'utilisateur appuie simplement sur Entrée
+REM Utiliser l'extension js si vide
 if "%extensionOutputInput%"=="" (
     set "extensionOutputInput=js"
 )
 
+
 REM Ajouter du texte au fichier Output (modifier selon vos besoins)
-echo let !fileNameOutput! = [ > !parentFolderName!.!extensionOutputInput!
+echo let _!fileNameOutput! = [ > !parentFolderName!.!extensionOutputInput!
+
+    REM Obtenir le chemin relatif
+    set "path=/!fileNameOutput!/"
 
 for /r %%i in (*.%extensionNameInput%) do (
     set "filePath=%%i"
     set "fileName=%%~nxi"
-	
-	REM Ajouter du texte au fichier Output (modifier selon vos besoins)
-    echo { set: '!fileNameOutput!', name: '%%~ni', file: '%%~dpnxi', position: { x: 0, y: 0, z: 0 } }, >> !fileNameOutput!.!extensionOutputInput!
 
+    set "width=0"
+    set "height=0"
+    REM Utiliser ImageMagick pour obtenir la taille de l'image
+    for /f "tokens=2 delims==x " %%a in ('magick identify -format "%%w==%%h" "%%i"') do (
+        set "width=%%a"
+        set "height=%%b"
+    )
+
+    REM Utiliser forfiles pour obtenir la taille du fichier en kilooctets
+    for /f %%a in ('forfiles /m "%%~nxi" /c "cmd /c echo @fsize"') do (
+        set "fileSize=%%a"
+        set /a fileSizeKB=fileSize/1024
+    )
+
+    REM Ajouter du texte au fichier Output (modifier selon vos besoins)
+    echo { set: '!fileNameOutput!', name: '%%~ni', file: '!path!%%~nxi', sizes:{ width: !width!, height: !height!, ko: !fileSizeKB! } }, >> !fileNameOutput!.!extensionOutputInput!
 )
 
 REM Ajouter du texte au fichier Output (modifier selon vos besoins)
 echo ] >> !fileNameOutput!.!extensionOutputInput!
-echo export { !fileNameOutput! } >> !fileNameOutput!.!extensionOutputInput!
-
+echo export { _!fileNameOutput! } >> !fileNameOutput!.!extensionOutputInput!
+pause
